7db83543f668d1e4cbc3c0ce445a05ebd57ddac2
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-04-15 11:07:30 -0300

    Initial commit

---
+++ app/src/main/java/br/com/itb/charity/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.itb.charity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import br.com.itb.charity.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case 1:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case 2:
+                    replaceFragment(new CatalogoFragment());
+                    break;
+                case 3:
+                    replaceFragment(new ConfigFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
