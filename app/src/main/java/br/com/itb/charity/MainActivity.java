caad97cdd8cc0cffff6eb669b37e729862dc9602
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-04-23 10:32:53 -0300

    foi porraaaaaaaaaaaaaaaaaaaaaaaaa

--- app/src/main/java/br/com/itb/charity/MainActivity.java
+++ app/src/main/java/br/com/itb/charity/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import br.com.itb.charity.databinding.ActivityMainBinding;
+import br.com.itb.charity.fragments.CatalogoFragment;
+import br.com.itb.charity.fragments.ConfigFragment;
+import br.com.itb.charity.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +28,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            switch (item.getItemId()) {
-                case R.id.menu_home:
-                    replaceFragment(new HomeFragment());
-                    break;
-                case R.id.menu_catalogo:
-                    replaceFragment(new CatalogoFragment());
-                    break;
-                case R.id.menu_config:
-                    replaceFragment(new ConfigFragment());
-                    break;
-            }
-
-
-            return true;
-        });
+        initNavgation();
     }
 
-    private void replaceFragment(Fragment fragment) {
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
+    private void  initNavgation() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
     }
+
+
 }
